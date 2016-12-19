ba446ce13aa54386794f55a40d737289d5a37993
Jeremy Schomery <jeremy.schomery@yahoo.com>
2016-12-19 14:08:24 +0330

    fixes #3

--- common.js
+++ common.js
@@ -12,3 +12,18 @@ chrome.runtime.onMessage.addListener((request, sender, response) => {
     return true;
   }
 });
+
+chrome.storage.local.get('version', (obj) => {
+  let version = chrome.runtime.getManifest().version;
+  if (obj.version !== version) {
+    window.setTimeout(() => {
+      chrome.storage.local.set({version}, () => {
+        chrome.tabs.create({
+          url: 'http://add0n.com/youtube-hover.html?version=' +
+            version + '&type=' +
+            (obj.version ? ('upgrade&p=' + obj.version) : 'install')
+        });
+      });
+    }, 3000);
+  }
+});
